38a46766b92bf13f0d994df25ff010a4f8401055
DavidSMoyer <43046866+DavidSMoyer@users.noreply.github.com>
2019-11-18 10:17:13 -0600

    Add files via upload

---
+++ php/form1.php
@@ -0,0 +1,13 @@
+﻿<!DOCTYPE html>
+
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+    <title>Simple Form</title>
+</head>
+<body>
+	Welcome, <?php echo $_GET["name"]; ?>, to the website!</br>
+	Check your email, <?php echo $_GET["email"]; ?>, for updates.
+</body>
+</html>
+
